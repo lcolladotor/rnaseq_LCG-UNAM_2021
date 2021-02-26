0efdca94b32d777e2e386bb75610acb82b82f711
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-02-26 08:57:08 -0600

    Actualizar ejercicios de revisión

--- 06_conclusion.R
+++ 06_conclusion.R
@@ -1,1 +1,7 @@
+## ----"speaqeasy_data"-------------------------------------------------------------------------------------------------------------------------------------
+speaqeasy_data <- file.path(tempdir(), "rse_speaqeasy.RData")
+download.file("https://github.com/LieberInstitute/SPEAQeasy-example/blob/master/rse_speaqeasy.RData?raw=true", speaqeasy_data, mode = "wb")
+library("SummarizedExperiment")
+load(speaqeasy_data, verbose = TRUE)
+rse_gene
 
